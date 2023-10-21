577339ae283550111c17afe7fc941cfc2b7ec103
Ankit Shukla <ankit2099shukla@gmail.com>
2023-10-21 15:16:52 +0530

    Node.js typescript express eslint setup

---
+++ backend/.eslintrc.js
@@ -0,0 +1,27 @@
+// was not needed because eslint extention is installed in vsCode
+
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
