c357aa975853d8b9a6315566ad197723491d6911
Ankit Shukla <ankit2099shukla@gmail.com>
2023-10-22 00:05:17 +0530

    Improve Error handling with httpErrors and create update, delete routes in notesRouter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { Error } from "mongoose";
 import notesRouter from "./routes/notes";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 // middleware to set up express to accept json bodies in request
@@ -11,14 +12,18 @@ app.use("/api/notes", notesRouter);
 
 // ----Error Handlers----
 app.use((req, res, next) => {
-  next(new Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   let errorMessage = "An unknown error occured";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -1,7 +1,18 @@
 import express from "express";
 import NoteModel from "../models/note.model";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 const router = express.Router();
+
+interface NoteBody {
+  title?: string;
+  text?: string;
+}
+
+interface PostNoteRequest {
+  body: NoteBody;
+}
 
 router
   .route("/")
@@ -13,11 +24,15 @@ router
       next(error);
     }
   })
-  .post(async (req, res, next) => {
-    const title = req.body.title;
-    const text = req.body.text;
+
+  .post(async (req: PostNoteRequest, res, next) => {
+    const { title, text } = req.body;
 
     try {
+      if (!title) {
+        throw createHttpError(400, "Note must have a title");
+      }
+
       const note = await NoteModel.create({
         title,
         text,
@@ -31,14 +46,78 @@ router
     }
   });
 
-router.route("/:noteId").get(async (req, res, next) => {
-  const noteId = req.params.noteId;
-  try {
-    const note = await NoteModel.findById(noteId).exec();
-    res.status(200).json(note);
-  } catch (error) {
-    next(error);
-  }
-});
+interface PatchNoteRequest {
+  params: { noteId: string };
+  body: NoteBody;
+}
+
+router
+  .route("/:noteId")
+  .get(async (req, res, next) => {
+    const noteId = req.params.noteId;
+    try {
+      if (!mongoose.isValidObjectId(noteId)) {
+        throw createHttpError(400, "Invalid note Id");
+      }
+
+      const note = await NoteModel.findById(noteId).exec();
+
+      if (!note) {
+        throw createHttpError(404, "Note not found");
+      }
+
+      res.status(200).json(note);
+    } catch (error) {
+      next(error);
+    }
+  })
+
+  .patch(async (req: PatchNoteRequest, res, next) => {
+    const noteId = req.params.noteId;
+    const newTitle = req.body.title;
+    const newText = req.body.text;
+    try {
+      if (!mongoose.isValidObjectId(noteId)) {
+        throw createHttpError(400, "Invalid note id");
+      }
+      if (!newTitle) {
+        throw createHttpError(400, "Note must have a title");
+      }
+
+      const updatedNote = await NoteModel.findByIdAndUpdate(
+        noteId,
+        { title: newTitle, text: newText },
+        { new: true }
+      );
+
+      if (!updatedNote) {
+        throw createHttpError(404, "Note not found");
+      }
+
+      res.status(200).json(updatedNote);
+    } catch (error) {
+      next(error);
+    }
+  })
+
+  .delete(async (req, res, next) => {
+    const noteId = req.params.noteId;
+
+    try {
+      if (!mongoose.isValidObjectId(noteId)) {
+        throw createHttpError(400, "Invalid note id");
+      }
+
+      const deletedNote = await NoteModel.findByIdAndDelete(noteId);
+
+      if (!deletedNote) {
+        throw createHttpError(404, "Note not found");
+      }
+
+      res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
+  });
 
 export default router;
