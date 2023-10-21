14b0f166650e96b4ded129e431b3c866814ee1fb
Ankit Shukla <ankit2099shukla@gmail.com>
2023-10-21 17:29:30 +0530

    Add middlewares for handling errors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,28 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import NoteModel from "./models/note.model";
+import { Error } from "mongoose";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const notes = await NoteModel.find().exec();
-  res.status(200).json(notes);
+app.get("/", async (req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(new Error("Endpoint not found"));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  let errorMessage = "An unknown error occured";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
 });
 
 export default app;
